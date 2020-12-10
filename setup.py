de30f124a202c2d4bb1cec9fa128f8f766e774f1
taikob <taisukekbys@gmail.com>
2020-12-10 11:38:23 +0900

    adapt to pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="color",
+    version="1.0",
+    author="Taisuke Kobayashi",
+    author_email="taisukekbys@gmail.com",
+    description="color is my own python package",
+    long_description="color",
+    long_description_content_type="text/markdown",
+    url="https://github.com/taikob",
+    packages=setuptools.find_packages()
+)
